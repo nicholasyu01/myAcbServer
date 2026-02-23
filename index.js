fd2d6dda396e7966d99fa0c73b567c9c12f68ce7
Nicholas Yu <nicholas.yu@sap.com>
2026-02-22 16:19:40 -0800

    add local

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 const cors = require('cors');
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5175',
     methods: ['GET', 'POST', 'OPTIONS'],
 }));
 const multer = require('multer');
